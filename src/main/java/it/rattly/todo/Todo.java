2d1d2d4a5b40611d69cd10d8d1987432b1783fbe
Rattlyy <giovannimariamazzone@gmail.com>
2022-04-19 16:50:20 +0200

    ciaone

--- src/main/java/it/rattly/todo/Todo.java
+++ src/main/java/it/rattly/todo/Todo.java
@@ -1,11 +1,7 @@
 package it.rattly.todo;
 
-import gg.jte.ContentType;
-import gg.jte.TemplateEngine;
-import gg.jte.resolve.DirectoryCodeResolver;
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
-import io.javalin.plugin.rendering.template.JavalinJte;
 import it.rattly.todo.controllers.TodoController;
 import it.rattly.todo.db.Jooq;
 import it.rattly.todo.db.generated.tables.daos.TodosDao;
@@ -13,7 +9,6 @@ import it.rattly.todo.views.TodoView;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
 import java.util.Collections;
 
 import static io.javalin.apibuilder.ApiBuilder.crud;
@@ -47,7 +42,6 @@ public class Todo {
         Jooq jooq = new Jooq(host, port, database, username, password);
         TodosDao todosDao = new TodosDao(jooq.settings());
 
-        JavalinJte.configure(createTemplateEngine());
         Javalin app = Javalin.create(config -> {
             config.enableWebjars();
             config.enableCorsForAllOrigins();
@@ -69,19 +63,9 @@ public class Todo {
         app.get("/", ctx -> ctx.render("index.jte", Collections.singletonMap("todoView", new TodoView(todosDao))));
     }
 
-    private static TemplateEngine createTemplateEngine() {
-        if (IS_DEV) {
-            DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(Path.of("src", "main", "jte"));
-            return TemplateEngine.create(codeResolver, ContentType.Html);
-        } else {
-            return TemplateEngine.createPrecompiled(Path.of("jte-classes"), ContentType.Html);
-        }
-    }
-
     private static int getHerokuAssignedPort() {
         String herokuPort = System.getenv("PORT");
 
-        System.out.println("port=" + herokuPort);
         if (herokuPort != null) {
             return Integer.parseInt(herokuPort);
         }
